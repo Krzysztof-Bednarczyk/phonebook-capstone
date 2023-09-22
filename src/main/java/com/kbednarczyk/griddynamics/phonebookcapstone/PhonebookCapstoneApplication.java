31bec3616f234c4330f162629e0039224a655006
Krzysztof Bednarczyk <krisu876@gmail.com>
2023-09-22 16:27:27 +0200

    Initial commit

---
+++ src/main/java/com/kbednarczyk/griddynamics/phonebookcapstone/PhonebookCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.kbednarczyk.griddynamics.phonebookcapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebookCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonebookCapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kbednarczyk/griddynamics/phonebookcapstone/PhonebookCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbednarczyk.griddynamics.phonebookcapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonebookCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
